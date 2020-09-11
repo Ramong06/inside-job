04c5631138990f2e95767416906d085449dde1db
Tolu Alimi <tolualimi05@gmail.com>
2020-09-11 15:52:37 -0500

    Added Search Form and put api call for search in search component

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import SearchForm from "../SearchForm";
+import API from "../../utils/FinanceAPI";
 
-function Search(props) {
+function Search() {
+    const [search, setSearch] = useState("");
+    const [companyName, setCompanyName] = useState("");
+    const [ticker, setTicker]
+
+    useEffect(() => {
+        if (!search)
+            return;
+
+        API.companyProfile(search)
+            .then(res => {
+                if (res.length === 0) {
+                    throw new Error("No results found.");
+                }
+                setTicker(res[0]["symbol"]);
+                setCompanyName(res[0]["companyName"]);
+            });
+    }, [search]);
+
+    API.companyProfile("Apple")
+            .then(res => {
+                if (res.length === 0) {
+                    throw new Error("No results found.");
+                }
+                setTicker(res[0]["symbol"]);
+                setCompanyName(res[0]["companyName"]);
+            });
+
+    const handleInputChange = event => {
+        setSearch(event.target.value);
+    };
+    
+    const handleFormSubmit = event => {
+        event.preventDefault();
+    };
+    
     return (
-        <div></div>
+        <div>
+            <SearchForm/>
+                <h1>Company: {companyName}</h1>
+                <h1>Ticker: {ticker}</h1>
+        </div>
     );
 }
 
