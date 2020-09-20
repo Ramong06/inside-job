c9f8492df38382ece7516fc4e18735de7e07f2cf
Tolu Alimi <tolualimi05@gmail.com>
2020-09-19 20:10:43 -0500

    Edited components so searchform carries out api and redirects to results page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Redirect, useHistory } from "react-router-dom";
 import Company from "./pages/Company";
 import Home from "./pages/Home";
 import Results from "./pages/Results";
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+function App() {
+  const [searchData, setSearchData] = useState({});
+
+  const handleSearchResults = (searchData, history) => {
+    setSearchData(searchData);
+
+    debugger
+
+    history.push("results");
+  };
+
     return (
       <Router>
         <Route exact path="/">
-          <Home />
+          <Home handleSearchResults={handleSearchResults} />
+        </Route>
+        <Route exact path="/results">
+          <Results searchData={searchData}/>
+        </Route>
+        <Route exact path="/company/:ticker">
+          <Company />
         </Route>
       </Router>
     );
   }
-}
 
 export default App;
---
+++ client/src/pages/Results/index.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+
+
+function Results({ searchData }) {
+    console.log(searchData);
+
+    return (
+        <React.Fragment>
+            {searchData.length ? <div>
+                <ul>
+                    {searchData.map(company => {
+                        return (
+                            <li>
+                                {company.name}
+                            </li>
+                        );
+                    })}
+                    
+                </ul>
+            </div> : <div>
+                    No Results
+            </div>}
+        </React.Fragment>
+    );
+}
+
+export default Results;
