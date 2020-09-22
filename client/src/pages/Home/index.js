cda21574fe09433f681962fe962645bed7a25489
Mina G <mghaffar89@gmail.com>
2020-09-21 19:37:25 -0500

    fixing the navbar

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import SearchForm from "../../components/SearchForm";
-import NavBar from "../../components/Navbar";
+import Navbar from "../../components/Navbar";
 import API from "../../utils/FinanceAPI";
 
 function Home({ handleSearchResults }) {
   return (
+    <Navbar></Navbar>
     <div>
       <SearchForm handleSearchResults={handleSearchResults} />
     </div>
