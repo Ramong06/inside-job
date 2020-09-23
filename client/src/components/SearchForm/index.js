1cb1a59a7e2c684514bea8ed2c2ac9f7e557c272
Ehson Raza <ehson.raza1@outlook.com>
2020-09-22 21:29:04 -0500

    added News API with API key

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -2,38 +2,36 @@ import React, { useState } from "react";
 import API from "../../utils/FinanceAPI";
 import { useHistory } from "react-router-dom";
 
+function SearchForm({ handleSearchResults }) {
+  const [search, setSearch] = useState("");
+  const history = useHistory();
 
-function SearchForm({handleSearchResults}) {
-    const [search, setSearch] = useState("");
-    const history = useHistory();
+  const handleInputChange = (event) => {
+    setSearch(event.target.value);
+  };
 
-    const handleInputChange = event => {
-        setSearch(event.target.value);
-    };
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
 
-    const handleFormSubmit = event => {
-        event.preventDefault();
-        
-        API.companyProfile(search)
-            .then(res => {
-                if (res.length === 0) {
-                    throw new Error("No results found.");
-                }
-                handleSearchResults(res.data, history);
-            });
-    };
+    API.companyProfile(search).then((res) => {
+      if (res.length === 0) {
+        throw new Error("No results found.");
+      }
+      handleSearchResults(res.data, history);
+    });
+  };
 
-    return (
-            <div className="form-group">
-                <input
-                    value={search}
-                    onChange={handleInputChange}
-                    name="results"
-                    className="form-control"
-                />
-                <button onClick={handleFormSubmit}>Submit</button>
-            </div>
-    );
+  return (
+    <div className="form-group">
+      <input
+        value={search}
+        onChange={handleInputChange}
+        name="results"
+        className="form-control"
+      />
+      <button onClick={handleFormSubmit}>Submit</button>
+    </div>
+  );
 }
 
 export default SearchForm;
