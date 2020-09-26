ef8f93b120faa6e42c0b166f4a4f3983f83d9a03
Tolu Alimi <tolualimi05@gmail.com>
2020-09-26 14:18:09 -0500

    Mapped searchData components to Results page

--- client/src/pages/Results/index.js
+++ client/src/pages/Results/index.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import API from "../../utils/FinanceAPI";
 import SearchForm from "../../components/SearchForm";
 import Navbar from "../../components/Navbar";
 
 function Results({ searchData, handleSearchResults }) {
+
+  console.log(searchData);
   return (
     <React.Fragment>
       <Navbar />
@@ -14,6 +17,9 @@ function Results({ searchData, handleSearchResults }) {
             <li key={company.symbol}>
               <Link to={"/company/" + company.symbol}>
                 <strong>{company.name}</strong>
+                <p>{company.symbol}</p>
+                <p>{company.industry}</p>
+                <p>{company.description}</p>
               </Link>
             </li>
           ))}
