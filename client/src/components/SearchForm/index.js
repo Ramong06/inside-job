d6fe801485e8505146718d04857b70b7513cab1e
Ehson Raza <ehson.raza1@outlook.com>
2020-09-25 22:45:58 -0500

    resolving merge conflicts 2

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -23,20 +23,6 @@ function SearchForm({ handleSearchResults }) {
   };
 
   return (
-<<<<<<< HEAD
-    <div className="container">
-      <div className="form">
-        <input
-          value={search}
-          onChange={handleInputChange}
-          name="results"
-          className="searchbox"
-        />
-        <button className="Searchbutton" onClick={handleFormSubmit}>
-          Search
-        </button>
-      </div>
-=======
     <div className="form">
       <input
         value={search}
@@ -48,7 +34,6 @@ function SearchForm({ handleSearchResults }) {
         <span class="glyphicon glyphicon-name"></span>
         Search
       </button>
->>>>>>> c3a78eaa72090441af037594464c3b0ac03eb6ec
     </div>
   );
 }
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -7,14 +7,9 @@ import "./style.css";
 function Home({ handleSearchResults }) {
   return (
     <div>
-<<<<<<< HEAD
-      <div class="main">
-        <Navbar />
-=======
       <Navbar />
       <div className="container">
         <img src={require("./dude.jpg")} height="200px" width="200px" />
->>>>>>> c3a78eaa72090441af037594464c3b0ac03eb6ec
         <SearchForm handleSearchResults={handleSearchResults} />
       </div>
     </div>
