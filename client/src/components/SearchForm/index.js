acd47ec38e246eec652f4f943ac780aa42e45bd2
Tolu Alimi <tolualimi05@gmail.com>
2020-09-26 14:16:22 -0500

    Updated searchform to map through second api call and add companyList to handlesearchresults

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import API from "../../utils/FinanceAPI";
 import { useHistory } from "react-router-dom";
-import "./index.css";
+import "./style.css";
 
 function SearchForm({ handleSearchResults }) {
   const [search, setSearch] = useState("");
+  const [searchData, setSearchData] = useState([]);
   const history = useHistory();
 
   const handleInputChange = (event) => {
@@ -14,13 +15,34 @@ function SearchForm({ handleSearchResults }) {
   const handleFormSubmit = (event) => {
     event.preventDefault();
 
-    API.companyProfile(search).then((res) => {
-      if (res.length === 0) {
+    // Replace with call to database
+    API.tickerSearch(search).then((results) => {
+      if (results.length === 0) {
         throw new Error("No results found.");
       }
-      handleSearchResults(res.data, history);
+
+      const companyPromises = results.data.map((company) => API.companyProfile(company.symbol));
+      Promise.all(companyPromises).then((companies) => {
+        console.log(companies);
+
+        const companyList = companies.map((newCompany) => ({
+          name: newCompany.data[0].companyName,
+          symbol: newCompany.data[0].symbol,
+          description: newCompany.data[0].description,
+          industry: newCompany.data[0].industry,
+          website: newCompany.data[0].website,
+          sector: newCompany.data[0].sector,
+          image: newCompany.data[0].image,
+        }));
+        setSearchData(companyList);
+        console.log(companyList);
+        debugger
+        handleSearchResults(companyList, history);
+      });
     });
   };
+    
+
 
   return (
     <div className="form">
