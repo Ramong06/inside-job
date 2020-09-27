2322a09751394a95ff054146284c900eb11dee01
Tolu Alimi <tolualimi05@gmail.com>
2020-09-27 18:15:12 -0500

    Wrote the API calls for company profile and income statement on the company page

--- client/src/pages/Company/index.js
+++ client/src/pages/Company/index.js
@@ -5,11 +5,25 @@ import API from "../../utils/FinanceAPI";
 import Navbar from "../../components/Navbar";
 
 function Company({ handleSearchResults }) {
-  const [company, setCompany] = useState({});
+  const [companyData, setCompanyData] = useState({});
+  const [financeData, setFinanceData] = useState({});
+  const [profile, setProfile] = useState({});
+
   const { ticker } = useParams();
 
   // Call APIs and retrieve company information from the databases
+  
+  // If we have the ticker symbol for the company then call the financial modeling APIs and look up company by ids in database
+  if (!(ticker[0] >= "0" && ticker[0] <= "9")) {
+    API.companyProfile(ticker).then(res => setProfile(res));
+    API.incomeStatement(ticker).then(res => setFinanceData(res));
 
+    // Set call to CompanyData by Ticker
+  }
+  else {
+    // Call CompanyData by id
+  }
+  
   return (
     // Format Components (Chart, Article Headlines, Ratings, Description, Salary etc.)
     <div>
--- controllers/companyController.js
+++ controllers/companyController.js
@@ -4,8 +4,13 @@ const db = require("../models");
 module.exports = {
   //TODO Need to add FindAll
   //Grabs the Salary info from the Mongoose model using the ticker -> Returns and Object
-  findByOne: function (req, res) {
+  findByTicker: function (req, res) {
     db.Salary.findOne({ ticker: req.params.ticker })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  findById: function (req, res) {
+    db.Salary.findOne({ id: req.params.ticker })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
