f09da759890d03781e5c2dd297797f87f11bdc25
Tolu Alimi <tolualimi05@gmail.com>
2020-09-28 00:16:42 -0500

    Updated company model

--- models/company.js
+++ models/company.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const companySchema = new Schema({
-  ticker: { type: String },
   name: { type: String },
-  salary: { type: Object },
   rating: { type: String },
-  review: { type: String },
+  reviews: { type: String },
+  ticker: { type: String },
+  salary: { type: Object },
 });
 
 const Company = mongoose.model("Company", salarySchema);
