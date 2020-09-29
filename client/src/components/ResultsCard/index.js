3e154c5e48a3642de177700b4b0453b61b43630e
Tolu Alimi <tolualimi05@gmail.com>
2020-09-29 11:59:16 -0500

    Added ternary for company description

--- client/src/components/ResultsCard/index.js
+++ client/src/components/ResultsCard/index.js
@@ -2,13 +2,8 @@ import React from "react";
 import "./style.css";
 
 function ResultsCard(props) {
-  let description =
-    props.company.description.length > 250
-      ? props.company.description + "..."
-      : props.company.description;
   return (
     <div className="row justify-content-center">
-
       <div className="card mb-3" style={{ width: "900px" }}>
         <div className="row no-gutters">
           <div className="col-md-4">
@@ -24,7 +19,18 @@ function ResultsCard(props) {
               <h5 className="card-title">{props.company.name}</h5>
             </div>
           </div>
-          {/* <div className="col-md-12"></div> */}
+          <div className="col-md-12">
+            {props.company.description ? <p className="card-text">
+              {props.company.description.substring(0, 300) + " ..."}
+            </p> : <p></p>}
+            <p className="card-text">
+              {props.company.symbol} - {props.company.industry} -{" "}
+              {props.company.sector}
+            </p>
+            <a href={props.company.website} className="card-link">
+              Website
+            </a>
+          </div>
         </div>
       </div>
     </div>
@@ -32,7 +38,3 @@ function ResultsCard(props) {
 }
 
 export default ResultsCard;
-
-// var snippetText = {props.company.description};
-//        if (snippetText.length > 200) {
-//          snippetText = snippetText.slice(0, 200);}
