1ea88ccd4ccf0918cd6e8c210008174beca47c50
Ehson Raza <ehson.raza1@outlook.com>
2020-09-29 11:32:17 -0500

    added styling, description to the results card

--- client/src/components/ResultsCard/index.js
+++ client/src/components/ResultsCard/index.js
@@ -8,7 +8,6 @@ function ResultsCard(props) {
       : props.company.description;
   return (
     <div className="row justify-content-center">
-
       <div className="card mb-3" style={{ width: "900px" }}>
         <div className="row no-gutters">
           <div className="col-md-4">
@@ -24,7 +23,18 @@ function ResultsCard(props) {
               <h5 className="card-title">{props.company.name}</h5>
             </div>
           </div>
-          {/* <div className="col-md-12"></div> */}
+          <div className="col-md-12">
+            <p className="card-text">
+              {props.company.description.substring(0, 300) + " ..."}
+            </p>
+            <p className="card-text">
+              {props.company.symbol} - {props.company.industry} -{" "}
+              {props.company.sector}
+            </p>
+            <a href={props.company.website} className="card-link">
+              Website
+            </a>
+          </div>
         </div>
       </div>
     </div>
