ef2611284b229470fce5cc8e4805d6a53a6c94a6
Tolu Alimi <tolualimi05@gmail.com>
2020-09-30 03:37:32 -0500

    Fixed error where companies without data were crashing the site

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -25,16 +25,18 @@ function SearchForm({ handleSearchResults }) {
         API.companyProfile(company.symbol)
       );
       Promise.all(companyPromises).then((companies) => {
-
-        const companyList = companies.map((newCompany) => ({
-          name: newCompany.data[0].companyName,
-          symbol: newCompany.data[0].symbol,
-          description: newCompany.data[0].description,
-          industry: newCompany.data[0].industry,
-          website: newCompany.data[0].website,
-          sector: newCompany.data[0].sector,
-          image: newCompany.data[0].image,
-        }));
+        const companyList = companies
+          .filter(newCompany => newCompany.data.length)
+          .map((newCompany) => ({
+            name: newCompany.data[0].companyName,
+            symbol: newCompany.data[0].symbol,
+            description: newCompany.data[0].description,
+            industry: newCompany.data[0].industry,
+            website: newCompany.data[0].website,
+            sector: newCompany.data[0].sector,
+            image: newCompany.data[0].image,
+          }));
+        
         setSearchData(companyList);
 
         handleSearchResults(companyList, history);
