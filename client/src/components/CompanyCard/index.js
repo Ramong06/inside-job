c0c9cc96850a298672c6b5d35cb5ee4f059a03ec
Ehson Raza <ehson.raza1@outlook.com>
2020-10-04 13:22:37 -0500

    made changes to render API data onto the page for both NEWS component and Company card

--- client/src/components/CompanyCard/index.js
+++ client/src/components/CompanyCard/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import "./style.css";
 
-function CompanyCard({profile}) {
-  return (profile.length ? 
+function CompanyCard({ data }) {
+  console.log("data", data);
+  return data ? (
     <div className="row justify-content-center">
       <div className="col col-lg-8">
         <div className="card-group">
           <div className="card" style={{ width: "100%" }}>
             <div className="card-body">
-              <h5 className="card-title">{profile.name}</h5>
-              <p className="card-text">{profile.description}</p>
-              <p className="card-text">{profile.salary}</p>
-              <p className="card-text">{profile.symbol} - {profile.sector} - {profile.industry}</p>
+              <h5 className="card-title">{data.companyName}</h5>
+              <p className="card-text">{data.description}</p>
+              {/* Needs to get populated from DB */}
+              <p className="card-text">{data.salary}</p>
+              <p className="card-text">
+                {data.symbol} - {data.sector} - {data.industry}
+              </p>
               <a href="#" className="card-link">
                 Link to company website
               </a>
@@ -19,7 +23,9 @@ function CompanyCard({profile}) {
           </div>
         </div>
       </div>
-    </div> : <div></div>
+    </div>
+  ) : (
+    <div></div>
   );
 }
 
